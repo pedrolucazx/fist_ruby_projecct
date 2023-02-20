e4c13dfda63539ea3e59bbf45b1cb48bfcf5ad9a
pedrolucazx <pedrolucastrainee@gmail.com>
2023-02-19 21:39:24 -0300

    final challenge

---
+++ challenge.rb
@@ -0,0 +1,8 @@
+puts 'Digite seu nome:'
+name = gets.chomp
+puts 'Digite seu sobrenome:'
+lastName = gets.chomp
+puts 'Digite sua idedae:'
+age = gets.chomp
+
+puts "#{name} #{lastName}, Seja Bem-Vindo! Sua idade é #{age}"
